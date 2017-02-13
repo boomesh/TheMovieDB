018fa25fe11e550738f33d5e2726889092a67d52
Boomesh Bohn <sumesh@bnotions.com>
2017-02-12 20:26:51 -0500

    Implemented set method in movie posters presentable implemented retrofit the movie db api

--- app/src/main/java/com/boomesh/domain/movieposters/MoviePostersPresenter.java
+++ app/src/main/java/com/boomesh/domain/movieposters/MoviePostersPresenter.java
@@ -2,16 +2,26 @@ package com.boomesh.domain.movieposters;
 
 import com.boomesh.domain.base.Presentable;
 
+import java.lang.ref.WeakReference;
+
 /**
  * In charge of determining when to show movie posters, as well as issuing the fetch requests
- *
+ * <p>
  * Created by sumesh on 2/12/17.
  */
 
 public class MoviePostersPresenter implements Presentable<MoviePostersViewable> {
 
+    private WeakReference<MoviePostersViewable> weakRef;
+
+    void fetchPosters() {
+        //TODO: talk to API and get top movies
+    }
+
+    //region Presentable
     @Override
     public void set(MoviePostersViewable viewable) {
-
+        weakRef = new WeakReference<MoviePostersViewable>(viewable);
     }
+    //endregion
 }
